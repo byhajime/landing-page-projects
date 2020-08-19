5ef3e6de8d476e2c085ec9d9e1dc1c8dd0c7898c
Hajime Honda <byhajime@live.jp>
2020-08-19 18:13:55 +0900

    'master'

--- Uploades/Theme/DATA/LEALES/functions.php
+++ Uploades/Theme/DATA/LEALES/functions.php
@@ -1,7 +1,7 @@
 <?php
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
- 'http://hoto-cocoa.com/LearningPage/wp-content/themes/LEALES/theme-update.json',
+ 'https://raw.githubusercontent.com/byhajime/landing-page-projects/master/Uploades/Theme/theme-update.json',
  __FILE__,
  'LEALES' //テーマ名
 );
